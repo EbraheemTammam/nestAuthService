af446a91140ac7e25cab80a238d858172d808a83
ebraheem <ebraheemtammam@gmail.com>
2026-05-31 00:51:30 +0300

    [FIX] remove skipping local guard upon public provided

--- src/auth/guards/local-auth.guard.ts
+++ src/auth/guards/local-auth.guard.ts
@@ -1,24 +1,11 @@
 import { ExecutionContext, Injectable } from "@nestjs/common";
-import { IS_PUBLIC_KEY } from "../decorators/public.decorator";
-import { Reflector } from "@nestjs/core";
 import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {
-    constructor(private readonly reflector: Reflector) {
-        super();
-    }
+    constructor() { super(); }
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
-        const isPublic = this.reflector.getAllAndOverride<boolean>(
-            IS_PUBLIC_KEY, 
-            [
-                context.getHandler(),
-                context.getClass(),
-            ]
-        );
-
-        if (isPublic) return true;
         return super.canActivate(context) as Promise<boolean>;
     }
 }
