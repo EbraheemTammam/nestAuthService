89dc22aee1de8dc05807b8a2df00a320a07ffbcb
ebraheem <ebraheemtammam@gmail.com>
2026-05-30 15:47:52 +0300

    [ADD] user find one and create methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,17 @@
-import { Injectable, NotImplementedException } from '@nestjs/common';
-import { UserDto } from './dtos/user.dto';
+import { Injectable } from '@nestjs/common';
+import { User } from 'src/generated/prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UserCreateDto } from './dtos/user-create.dto';
 
 @Injectable()
 export class UsersService {
-    async findOne(email: string): Promise<UserDto> {
-        throw new NotImplementedException()
+    constructor(private readonly prisma: PrismaService) {}
+    
+    async findOne(email: string): Promise<User | null> {
+        return this.prisma.user.findUnique({ where: { email } });
+    }
+
+    async create(data: UserCreateDto) {
+        await this.prisma.user.create({ data })
     }
 }
