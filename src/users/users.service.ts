f4244e3c17a085e77dbab09f69fe95be7780f21d
ebraheem <ebraheemtammam@gmail.com>
2026-05-30 16:08:21 +0300

    [UPDATE] hash user password while creation and add verify password method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Prisma, User } from 'src/generated/prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserCreateDto } from './dtos/user-create.dto';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class UsersService {
@@ -13,6 +14,14 @@ export class UsersService {
 
     async create(data: UserCreateDto): Promise<{ sucess: boolean, error?: string }> {
         try {
+            data.password = await argon2.hash(
+                data.password, {
+                    type: argon2.argon2id,
+                    memoryCost: 2 ** 16,
+                    timeCost: 3, 
+                }
+            );
+            
             await this.prisma.user.create({ data });
             return { sucess: true };
         } catch (error) {
@@ -27,5 +36,9 @@ export class UsersService {
         }
 
         return { sucess: false, error: 'An unkowm error occured' };
+    }
+
+    async verifyPassword(plainTextPassword: string, hashedPassword: string): Promise<boolean> {
+        return argon2.verify(hashedPassword, plainTextPassword);
     }
 }
