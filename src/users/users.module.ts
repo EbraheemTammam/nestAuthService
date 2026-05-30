2770adeb30ae6a2c87724b6e71e31d926fc238d2
ebraheem <ebraheemtammam@gmail.com>
2026-05-30 20:51:07 +0300

    [UPDATE] export users service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Module({
+  exports: [UsersService],
   providers: [UsersService]
 })
 export class UsersModule {}
